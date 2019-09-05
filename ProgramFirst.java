2374b2b0b9ad21a416901816506cc8753befa1b2
PrabuddhaBanerjee <51701112+PrabuddhaBanerjee@users.noreply.github.com>
2019-09-05 18:14:46 -0400

    Add files via upload

---
+++ ProgramFirst.java
@@ -0,0 +1,7 @@
+
+public class ProgramFirst {
+	public static void main(String[] args){
+		//Print the value of equation
+		System.out.println("Answer is: "+(((15.4*6.5)-(4.3*3))/(46.3-3.5)));
+	}
+}
