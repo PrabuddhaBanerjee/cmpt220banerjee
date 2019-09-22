7aa230aa18d7a56a3f6efc8f76d394070b059f66
Juan Arias <juan.arias@marist.edu>
2019-09-21 22:15:56 -0400

    Feedback

--- ProgramFirst.java
+++
@@ -1,7 +0,0 @@
-
-public class ProgramFirst {
-	public static void main(String[] args){
-		//Print the value of equation
-		System.out.println("Answer is: "+(((15.4*6.5)-(4.3*3))/(46.3-3.5)));
-	}
-}
--- labs/3/Lab3Program1.java
+++ labs/3/Lab3Program1.java
@@ -7,16 +7,20 @@ public class Lab3Program1 {
 		int num;
 		int sum=0;
 		int total = 0;
-		int avg;
+		double avg; // JA: This should be a double
 				
 		System.out.println("Enter a positive integer, the input ends with a 0:");
 		num = input.nextInt();
 		while(num!=0) {
-			num = input.nextInt();
+			// JA: Somewhere in here, you need to count the number of even numbers
+			if (num%2 == 0) count++;
 			sum+=num;
 			System.out.println(sum);
+			// JA: You also need to keep 
+			total++;
+			num = input.nextInt();
 		}
-		avg = sum/total;
+		avg = (double)sum/total;
 		System.out.println("The number of even numbers: "+count);
 		System.out.println("The total sum: "+sum);
 		System.out.println("The average is: "+avg);
