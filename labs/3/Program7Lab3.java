7060584abe34d0541e6df9e7f573c7f9ad778635
PrabuddhaBanerjee <51701112+PrabuddhaBanerjee@users.noreply.github.com>
2019-10-22 17:00:46 -0400

    Add files via upload

---
+++ labs/3/Program7Lab3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class JavaMyClass {
+	double p;
+	double pentagon(int n) {
+		//System.out.println(n);
+		//for (int i=n; i<0;i++) {
+			p = ((Math.pow(n,2)*3)-n)/2;
+			//System.out.println(n);
+		//}
+		
+		return p;
+	}
+	
+public static void main(String[] args) {
+	int num;
+	Scanner input = new Scanner(System.in);
+	num = input.nextInt();
+	JavaMyClass temp = new JavaMyClass();
+	while(temp.pentagon(num)!=0) {
+		System.out.println(temp.pentagon(num));
+		num--;
+	}
+	//System.out.println(temp.pentagon(num));
+	
+	input.close();
+}
+}
