26d68d8e4ab45b67c404edbf15f934c66c51a5cb
PrabuddhaBanerjee <51701112+PrabuddhaBanerjee@users.noreply.github.com>
2019-10-24 23:34:10 -0400

    Add files via upload

---
+++ labs/7/Circle.java
@@ -0,0 +1,51 @@
+public class Circle
+{
+    private double x;
+    private double y;
+    private double radius;
+    Circle(){
+      // Initialize instance variables
+      x = 0;
+      y = 0;
+      radius = 0;
+    }
+    Circle(double x, double y, double radius){
+      // Initialize instance variables
+      this.x = x;
+      this.y = y;
+      this.radius = radius;
+    }
+    public double getCenter()
+    {
+        // put your code here
+        //return x+","+y;
+        return x ;
+    }
+    public double getRadius(){
+      return radius;
+    }
+    public double getArea(){
+      return Math.PI * Math.pow(radius,2);
+    }
+    public double getPerimeter(){
+      return 2*Math.PI *radius ;
+    }
+    public boolean contains(double x1, double y1){
+      double check = Math.pow((Math.pow(x1-x,2)+Math.pow(y1-y,2)),0.5);
+      if(check<radius)
+        return true;
+      else return false;
+    }
+    public boolean contains(Circle circle){//c>a+b
+      double check = (Math.pow((Math.pow(this.x-circle.x,2)+Math.pow(this.y-circle.y,2)),0.5))+circle.radius;
+	 // return circle.contains(circle.x,circle.y);
+    	return this.radius> check;
+    }
+    public boolean overlaps(Circle circle){
+    	double a =Math.pow((Math.pow(this.x-circle.x,2)+Math.pow(this.y-circle.y,2)),0.5);
+    
+      return a<this.radius+circle.radius;
+    }
+    
+    
+}
---
+++ labs/7/Driver_circle.java
@@ -0,0 +1,11 @@
+class Driver_circle{
+  public static void main(String[] args) {
+    	Circle c1 = new Circle(2,2,5.5);
+    	System.out.println("Area of Circle c1:"+c1.getArea());
+    	System.out.println("Perimeter of Circle c1:"+c1.getPerimeter()); 
+    	System.out.println("Does c1 circle contains circle (3, 3)?"+c1.contains(3, 3));
+    	System.out.println("Does c1 circle contains Circle(4, 5, 10.5)?"+c1.contains(new Circle(4, 5, 10.5)));
+    	System.out.println("Does c1 circle overlaps Circle(3, 5, 2.3)?"+c1.overlaps(new Circle(3, 5, 2.3)));
+    	
+    }
+}
