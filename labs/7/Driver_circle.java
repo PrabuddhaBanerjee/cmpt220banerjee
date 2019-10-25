0b5e3025396958c508342a03c710bcd244559335
PrabuddhaBanerjee <51701112+PrabuddhaBanerjee@users.noreply.github.com>
2019-10-25 15:24:20 -0400

    Update Driver_circle.java

--- labs/7/Driver_circle.java
+++ labs/7/Driver_circle.java
@@ -5,7 +5,9 @@ class Driver_circle{
     	System.out.println("Perimeter of Circle c1:"+c1.getPerimeter()); 
     	System.out.println("Does c1 circle contains circle (3, 3)?"+c1.contains(3, 3));
     	System.out.println("Does c1 circle contains Circle(4, 5, 10.5)?"+c1.contains(new Circle(4, 5, 10.5)));
-    	System.out.println("Does c1 circle overlaps Circle(3, 5, 2.3)?"+c1.overlaps(new Circle(3, 5, 2.3)));
-    	
+    	//2 Methods for doing a same thing
+      //System.out.println("Does c1 circle overlaps Circle(3, 5, 2.3)?"+c1.overlaps(new Circle(3, 5, 2.3)));
+    	Circle case3Circle = new Circle(3, 5, 2.3);
+      System.out.println("Does c1 circle overlaps Circle(3, 5, 2.3)?"+c1.overlaps(case3Circle));
     }
 }
