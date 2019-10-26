425095d312c7fbb85c28d8e99c87442e0e3e5263
PrabuddhaBanerjee <51701112+PrabuddhaBanerjee@users.noreply.github.com>
2019-10-26 07:44:10 +0530

    Update Driver_circle.java

--- labs/7/Driver_circle.java
+++ labs/7/Driver_circle.java
@@ -1,8 +1,8 @@
 class Driver_circle{
   public static void main(String[] args) {
     	Circle c1 = new Circle(2,2,5.5);
-    	System.out.println("Area of Circle c1:"+c1.getArea());
-    	System.out.println("Perimeter of Circle c1:"+c1.getPerimeter()); 
+    	System.out.println("Area of Circle c1:"+c1.getArea()+" sq units");
+    	System.out.println("Perimeter of Circle c1:"+c1.getPerimeter()+" units"); 
     	System.out.println("Does c1 circle contains circle (3, 3)?"+c1.contains(3, 3));
     	System.out.println("Does c1 circle contains Circle(4, 5, 10.5)?"+c1.contains(new Circle(4, 5, 10.5)));
     	//2 Methods for doing a same thing
